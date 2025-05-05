a0e8036572d383b11e9d936761285ba0249af92e
Murat Can Ozdemir <can@reynencourt.com>
2025-05-05 21:59:03 +0200

    treE

--- src/download.sh
+++ src/download.sh
@@ -6,4 +6,5 @@ cd data &&
 curl -L -o raw/student-depression-dataset.zip\
   https://www.kaggle.com/api/v1/datasets/download/adilshamim8/student-depression-dataset
 unzip raw/student-depression-dataset.zip -d raw
-cd ..
+cd ../..
+tree -L 3
