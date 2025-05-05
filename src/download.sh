abe4dcc1f0f21c101237b3a52f88b08a9da560b3
Murat Can Ozdemir <can@reynencourt.com>
2025-05-05 21:46:46 +0200

    no assumptions

--- src/download.sh
+++ src/download.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd ~/TRON-demo/cicd-demo/mlops-demo/data &&
+cd data &&
 curl -L -o raw/student-depression-dataset.zip\
   https://www.kaggle.com/api/v1/datasets/download/adilshamim8/student-depression-dataset
 unzip raw/student-depression-dataset.zip -d raw
+cd ..
