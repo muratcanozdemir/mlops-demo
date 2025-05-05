4cafc09ab23163af63b86f5ebc9bc0a8d6d1dd3d
Murat Can Ozdemir <can@reynencourt.com>
2025-05-05 23:02:38 +0200

    Typo

--- src/feature_extract.py
+++ src/feature_extract.py
@@ -3,7 +3,7 @@ import pandas as pd
 from pathlib import Path
 
 
-data = pd.read_csv("data/preprocessed/student_depression_dataset_preprocessed.csv")
+data = pd.read_csv("data/preprocessed/student_depression_dataset_rocessed.csv")
 # Served its purpose in the reporting section, don't need for model training
 if 'Total Pressure' in data.columns:
     data.drop(columns=['Total Pressure'], inplace=True)
@@ -45,4 +45,4 @@ else:
 
 os.makedirs("data/model_ready", exist_ok=True)
 model_ready = pd.concat([X, y], axis=1)
-data.to_csv("data/model_ready/student_depression_dataset_model_ready.csv", index=False)
+model_ready.to_csv("data/model_ready/student_depression_dataset_model_ready.csv", index=False)
