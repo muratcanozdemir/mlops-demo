7f8639cf6e2bec51bcd8f32212c6b95ccf43f367
Murat Can Ozdemir <can@reynencourt.com>
2025-05-05 22:01:44 +0200

    arrange

--- src/download.sh
+++ src/download.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-mkdir -p data/raw && \
-mkdir -p data/preprocessed && \
-mkdir -p data/model_ready
-cd data &&
-curl -L -o raw/student-depression-dataset.zip\
+set -euo pipefail
+
+# Create necessary directories
+mkdir -p data/raw data/processed data/model_ready
+
+# Download and extract the dataset
+cd data
+curl -L -o raw/student-depression-dataset.zip \
   https://www.kaggle.com/api/v1/datasets/download/adilshamim8/student-depression-dataset
-unzip raw/student-depression-dataset.zip -d raw
-cd ../..
-tree -L 3
+unzip -o raw/student-depression-dataset.zip -d raw
