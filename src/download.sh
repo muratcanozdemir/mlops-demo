a24c56cdfad383998e5f82fc4839467ada795a6f
Murat Can Ozdemir <can@reynencourt.com>
2025-05-05 21:49:17 +0200

    add directories since they are not tracked

--- src/download.sh
+++ src/download.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+mkdir -p data/raw && \
+mkdir -p data/processed && \
+mkdir -p data/model_ready
 cd data &&
 curl -L -o raw/student-depression-dataset.zip\
   https://www.kaggle.com/api/v1/datasets/download/adilshamim8/student-depression-dataset
